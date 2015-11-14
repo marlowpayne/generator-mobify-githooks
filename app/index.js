f5060a423ad110858550b8669f17b400b5a5d984
Marlow Payne <marlow@mobify.com>
2015-11-14 15:44:47 -0800

    MVP to add npm scripts entry to package.json

--- app/index.js
+++ app/index.js
@@ -15,6 +15,14 @@ var GithooksGenerator = yeoman.generators.Base.extend({
 });
 
 GithooksGenerator.prototype.app = function app() {
+    var _getIndexForDependencyInsertion = function(fileString) {
+        var dependenciesIndex = fileString.lastIndexOf('dependencies');
+
+        var dependenciesString = fileString.slice(dependenciesIndex);
+
+        return dependenciesIndex + dependenciesString.indexOf('}');
+    };
+
     // Read existing package.json
     var packageJsonPath = './package.json';
     var packageJsonFile = this.readFileAsString(packageJsonPath);
@@ -22,18 +30,37 @@ GithooksGenerator.prototype.app = function app() {
     // Check that grunt-githooks isn't already in package.json
     if (!/"grunt-githooks"/g.test(packageJsonFile)) {
 
-        var dependenciesIndex = packageJsonFile.lastIndexOf('dependencies');
-
-        var dependenciesString = packageJsonFile.slice(dependenciesIndex);
-
-        var indexForDependencyInsertion = dependenciesIndex + dependenciesString.indexOf('}');
+        var indexForDependencyInsertion = _getIndexForDependencyInsertion(packageJsonFile);
 
         // Add grunt-githooks dependency to existing package.json
         packageJsonFile = packageJsonFile.slice(0, indexForDependencyInsertion - 1) + ',\n"grunt-githooks": "0.3.1"\n' + packageJsonFile.slice(indexForDependencyInsertion);
     }
 
     if (!/"scripts"/g.test(packageJsonFile)) {
-        // TODO: Add postinstall npm scripts
+        // Insert postinstall script after dependencies
+        var indexForMissingScriptsInsertion = _getIndexForDependencyInsertion(packageJsonFile) + 1;
+
+        // Be prepared for entries after dependencies
+        if (indexForMissingScriptsInsertion === ',') {
+            packageJsonFile = packageJsonFile.slice(0, indexForMissingScriptsInsertion + 1) + '\n"scripts": {"postinstall": "grunt githooks"},\n' + packageJsonFile.slice(indexForMissingScriptsInsertion);
+        } else {
+            packageJsonFile = packageJsonFile.slice(0, indexForMissingScriptsInsertion) + ',\n"scripts": {"postinstall": "grunt githooks"}\n' + packageJsonFile.slice(indexForMissingScriptsInsertion);
+        }
+    } else {
+        var scriptsIndex = packageJsonFile.lastIndexOf('scripts');
+        var scriptsString = packageJsonFile.slice(scriptsIndex);
+        // Check for postinstall script
+        if (!/"postinstall"/g.test(scriptsString)) {
+            // Add postinstall script
+            var indexForExistingScriptsInsertion = scriptsIndex + scriptsString.indexOf('}');
+
+            // Check for empty object case
+            if (packageJsonFile.charAt(indexForExistingScriptsInsertion - 1) === '{') {
+                packageJsonFile = packageJsonFile.slice(0, indexForExistingScriptsInsertion) + '\n"postinstall": "grunt githooks"\n' + packageJsonFile.slice(indexForExistingScriptsInsertion);
+            } else {
+                packageJsonFile = packageJsonFile.slice(0, indexForExistingScriptsInsertion - 1) + ',\n"postinstall": "grunt githooks"\n' + packageJsonFile.slice(indexForExistingScriptsInsertion);
+            }
+        }
     }
 
     // TODO: Write package.json file back out
