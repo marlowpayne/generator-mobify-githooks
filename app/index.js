1322fefaf79a788f7cd80ef851cbf6514177ac92
Marlow Payne <marlow@mobify.com>
2015-11-14 17:22:56 -0800

    I'm an idiot

--- app/index.js
+++ app/index.js
@@ -24,46 +24,17 @@ GithooksGenerator.prototype.app = function app() {
 
     // Read existing package.json
     var packageJsonPath = './package.json';
-    var packageJsonFile = this.readFileAsString(packageJsonPath);
+    var packageJsonString = this.readFileAsString(packageJsonPath);
+    var packageJson = JSON.parse(packageJsonString);
 
-    // Check that grunt-githooks isn't already in package.json
-    if (!/"grunt-githooks"/g.test(packageJsonFile)) {
+    packageJson.dependencies['grunt-githooks'] = '0.3.1';
 
-        var indexForDependencyInsertion = _getIndexForDependencyInsertion(packageJsonFile);
-
-        // Add grunt-githooks dependency to existing package.json
-        packageJsonFile = packageJsonFile.slice(0, indexForDependencyInsertion - 1) + ',\n"grunt-githooks": "0.3.1"\n' + packageJsonFile.slice(indexForDependencyInsertion);
-    }
-
-    if (!/"scripts"/g.test(packageJsonFile)) {
-        // Insert postinstall script after dependencies
-        var indexForMissingScriptsInsertion = _getIndexForDependencyInsertion(packageJsonFile) + 1;
-
-        // Be prepared for entries after dependencies
-        if (indexForMissingScriptsInsertion === ',') {
-            packageJsonFile = packageJsonFile.slice(0, indexForMissingScriptsInsertion + 1) + '\n"scripts": {"postinstall": "grunt githooks"},\n' + packageJsonFile.slice(indexForMissingScriptsInsertion);
-        } else {
-            packageJsonFile = packageJsonFile.slice(0, indexForMissingScriptsInsertion) + ',\n"scripts": {"postinstall": "grunt githooks"}\n' + packageJsonFile.slice(indexForMissingScriptsInsertion);
-        }
-    } else {
-        var scriptsIndex = packageJsonFile.lastIndexOf('scripts');
-        var scriptsString = packageJsonFile.slice(scriptsIndex);
-        // Check for postinstall script
-        if (!/"postinstall"/g.test(scriptsString)) {
-            // Add postinstall script
-            var indexForExistingScriptsInsertion = scriptsIndex + scriptsString.indexOf('}');
-
-            // Check for empty object case
-            if (packageJsonFile.charAt(indexForExistingScriptsInsertion - 1) === '{') {
-                packageJsonFile = packageJsonFile.slice(0, indexForExistingScriptsInsertion) + '\n"postinstall": "grunt githooks"\n' + packageJsonFile.slice(indexForExistingScriptsInsertion);
-            } else {
-                packageJsonFile = packageJsonFile.slice(0, indexForExistingScriptsInsertion - 1) + ',\n"postinstall": "grunt githooks"\n' + packageJsonFile.slice(indexForExistingScriptsInsertion);
-            }
-        }
-    }
+    packageJson.scripts = packageJson.scripts ? packageJson.scripts['postinstall'] = 'grunt githooks' : {
+        'postinstall': 'grunt githooks'
+    };
 
     // Write package.json file back out
-    this.writeFileFromString(packageJsonFile, packageJsonPath);
+    this.writeFileFromString(JSON.stringify(packageJson, null, 4), packageJsonPath); // 4 spaces as tabs
 
     // Copy githooks tasks/config/githooks.js and tasks/hooks/tasks.js
     this.copy('githooks.js', 'tasks/config/githooks.js');
